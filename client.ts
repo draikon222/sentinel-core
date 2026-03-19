8d2ebc9a7d287ed455158c62d2924a74be6a9347
draikon222 <draikon27@gmail.com>
2026-03-19 08:50:41 +0200

    Update client.ts

--- client.ts
+++ client.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
-// Această linie trimite fișierul tău HTML nou creat către vizitatori
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html')); 
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Sentinel Core operational pe portul ${port}`);
+  console.log('Sentinel Core Operational');
 });
